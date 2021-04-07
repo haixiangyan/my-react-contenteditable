9995577e013c8a946d41fedaa1a9aa605bc02baa
HaixiangYan <haixiang6123@gmail.com>
2021-04-07 21:09:37 +0800

    feat: 把光标放到最后

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -10,9 +10,41 @@ interface Props {
   onChange?: (e: ContentEditableEvent) => void
 }
 
+const replaceCaret = (el: HTMLElement) => {
+  // 创建光标
+  const cursor = document.createTextNode('')
+  el.appendChild(cursor)
+
+  // 判断是否选中
+  const isFocused = document.activeElement === el
+  if (!cursor || !cursor.nodeValue || !isFocused) return
+
+  // 将光标放到最后
+  const selection = window.getSelection()
+  if (selection !== null) {
+    const range = document.createRange()
+    range.setStart(cursor, cursor.nodeValue.length)
+    range.collapse()
+
+    selection.removeAllRanges()
+    selection.addRange(range)
+  }
+
+  // 重新 focus
+  if (el instanceof HTMLElement) el.focus()
+}
+
 class ContentEditable extends Component<Props> {
   lastHtml = this.props.value
   ref = createRef<HTMLDivElement>()
+
+  componentDidUpdate() {
+    if (!this.ref.current) return
+
+    this.lastHtml = this.props.value
+
+    replaceCaret(this.ref.current)
+  }
 
   emitEvent = (originalEvent: SyntheticEvent<any>) => {
     if (!this.ref.current) return
